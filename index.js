928977601e211b1a07a78191d73a6f91a9a9a875
AaronKolenda <kolenda.a@gmail.com>
2015-05-11 11:51:11 -0400

    adding edit header functionality

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var Header = Backbone.Model.extend({
 
   defaults: {
   	cover: "",
-    avatar: "",
+    avatar: "http://png.findicons.com/files/icons/2198/dark_glass/128/emoticon.png",
     name: "nameex",
     bio: "bioex",
     facebook: "fex",
@@ -42,7 +42,7 @@ var HeaderView = Backbone.View.extend({
 
   className: "header",
 
-initialize: function(model) {
+  initialize: function(model) {
   	this.model = model;
     this.render();
   },
@@ -52,7 +52,46 @@ initialize: function(model) {
   },
 
   edit: function(){
-    
+    $("#header-container").html("");
+    var editView = new EditHeaderView(this.model);
+    $("#header-container").append(editView.$el);
+  }
+
+});
+
+var EditHeaderView = Backbone.View.extend({
+
+  events: { "click #save": "save"},
+
+  className: "header",
+
+  initialize: function(model) {
+  	this.model = model;
+    this.render();
+  },
+
+  render: function() {
+    this.$el.html(templates.editHeaderInfo(this.model.viewDetails()));
+  },
+
+  save: function(){
+  	var cover = $("#cover").val();
+  	var avatar = $("#avatar").val();
+  	var name = $("#name").val();
+  	var bio = $("#bio").val();
+  	var facebook = $("#facebook").val();
+  	var twitter = $("#twitter").val();
+  	var linkedin = $("#linkedin").val();
+  	this.model.set("cover", cover);
+  	this.model.set("avatar", avatar);
+  	this.model.set("name", name);
+  	this.model.set("bio", bio);
+  	this.model.set("facebook", facebook);
+  	this.model.set("twitter", twitter);
+  	this.model.set("linkedin", linkedin);
+    $("#header-container").html("");
+    var editedHeader = new HeaderView(this.model);
+    $("#header-container").append(editedHeader.$el);
   }
 
 });
